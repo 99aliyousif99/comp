7a4c5d4c2412a726b3cccedfdc97e2c2d22b887f
99aliyousif99 <aljanabiali3@gmail.com>
2024-12-11 23:02:02 +0300

    commit

--- src/components/Storage/Storage.jsx
+++ src/components/Storage/Storage.jsx
@@ -4,13 +4,30 @@ import upload from "../../assets/upload.svg";
 import checkmark from "../../assets/vector (2).svg";
 
 const Storage = () => {
-
-  
-  
   return (
     <>
-      <div className="upload-container">
+      <div className="container">
+        <input type="file" className="upload" />
+        <div className="upload-container">
+          <div className="loadContainer">
+            <div className="loading">
+              <label htmlFor="">Loading</label>
+              <div className="loadBar"></div>
+              <div className="amountuploaded">s</div>
+            </div>
+          </div>
+          <div className="files">
+          <img src={checkmark} alt="" />
+          <div className="fileInfo">
+            <p>file name</p>
+            <p>files size</p>
+            
+          </div>
+          <button>delete</button>
+        </div>
+        </div>
         
+
       </div>
     </>
   );
