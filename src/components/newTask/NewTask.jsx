788608dce3735c5c4d54e0f6a0cb35c794185b5a
99aliyousif99 <aljanabiali3@gmail.com>
2024-12-15 14:32:42 +0300

    commit

--- src/components/newTask/NewTask.jsx
+++ src/components/newTask/NewTask.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./NewTask.css";
 import icon from "../../assets/icon.svg";
 import Frame from "../../assets/frame (1).svg";
 import checkmark from "../../assets/vector (2).svg";
-import { useState, useEffect } from "react";
 
 const NewTask = () => {
+  const fileInputRef = useRef(null);
+
+  const handleButtonClick = () => {
+    fileInputRef.current.click();
+  };
+
   return (
     <div className="container">
-      <button className="upload"><img src={Frame} alt="" /></button>
+      <button className="upload" onClick={handleButtonClick}>
+        <img src={Frame} alt="" />
+      </button>
+      <input
+        type="file"
+        ref={fileInputRef}
+        style={{ display: "none" }}
+      />
       <div className="content">
         <div className="uploadContainer">
           <div className="uploadInfo">
@@ -20,7 +32,6 @@ const NewTask = () => {
           </div>
         </div>
         <div className="files">
-            
           <div className="check">
             <img src={checkmark} alt="" />
           </div>
@@ -29,9 +40,10 @@ const NewTask = () => {
             <p>1 TB used</p>
           </div>
           <div className="gapping"></div>
-          <button className="delete"> <img src={icon} alt="" /></button>
+          <button className="delete">
+            <img src={icon} alt="" />
+          </button>
         </div>
-        
         <button className="goTo">go to downloads</button>
       </div>
     </div>
